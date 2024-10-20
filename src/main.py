3fdfd8da94d9fc1ebeda233b67d23a64b0362159
SketchQ <eserdnmez@gmail.com>
2024-10-20 22:30:37 +0300

    Main.py : added __main__

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from gui import Window
 
-if __name__ == "main":
+if __name__ == "__main__":
     win = Window(800, 600)  # Create a window with 800x600 pixels
     win.wait_for_close()    # Wait for the window close
 
